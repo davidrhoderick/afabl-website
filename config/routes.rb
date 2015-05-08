16c4d7bcd50066dd84fdf54f35ec4508591338c8
David Rhoderick <broken.stairs@gmail.com>
2015-05-08 18:22:37 +0200

    Adding static pages to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get '/' => 'static_pages#welcome'
+
+  get '/contact' => 'static_pages#contact'
+
+  get '/about' => 'static_pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -9,7 +13,7 @@ Rails.application.routes.draw do
   end
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'static_pages#welcome'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
