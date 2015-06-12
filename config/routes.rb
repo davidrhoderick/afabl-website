47015974277af1828e28e90b1277e99cb2e009a6
David Rhoderick <david@artlyticalmedia.com>
2015-06-13 01:42:43 +0200

    Removing static pages contact controller from routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get '/' => 'static_pages#welcome'
-  get '/contact' => 'static_pages#contact'
   get '/about' => 'static_pages#about'
   get '/contact' => 'contacts#new'
 
